02a9b080a2094cf4d6d33f2bd785259a6f7d8b1f
Christophe Lambin <christophe.lambin@gmail.com>
2024-12-09 00:04:09 +0100

    feat: remove need to have kubeseal installed locally: use github.com/bitnami-labs/sealed-secrets instead. (#4)

--- internal/cmd/seal.go
+++ internal/cmd/seal.go
@@ -1,15 +1,20 @@
 package cmd
 
 import (
+	"context"
+	"crypto/rsa"
 	"fmt"
+	"github.com/bitnami-labs/sealed-secrets/pkg/apis/sealedsecrets/v1alpha1"
+	"github.com/bitnami-labs/sealed-secrets/pkg/kubeseal"
 	"github.com/clambin/go-common/charmer"
 	"github.com/clambin/seals/internal/inventory"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"io"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
 	"log/slog"
 	"os"
-	"os/exec"
 	"path/filepath"
 )
 
@@ -28,21 +33,27 @@ var (
 			if err != nil {
 				return fmt.Errorf("unable to load ansible inventory file: %w", err)
 			}
-			return seal(inv, viper.GetViper(), charmer.GetLogger(cmd))
+			s := newKubeSealer(viper.GetString("controller-namespace"), viper.GetString("controller-namespace"))
+			return seal(s, inv, viper.GetViper(), charmer.GetLogger(cmd))
 		},
 	}
 )
 
-func seal(inv inventory.Inventory, v *viper.Viper, l *slog.Logger) error {
+// sealer interface so we can stub during unit testing
+type sealer interface {
+	seal(w io.Writer, r io.Reader, namespace string) error
+}
+
+func seal(s sealer, inv inventory.Inventory, v *viper.Viper, l *slog.Logger) error {
 	for _, secret := range inv.Secrets {
-		if err := maybeSeal(inv, secret, v, l.With("secret", secret.Source)); err != nil {
+		if err := maybeSeal(s, inv, secret, v, l.With("secret", secret.Source)); err != nil {
 			return fmt.Errorf("failed to seal %q: %w", secret.Source, err)
 		}
 	}
 	return nil
 }
 
-func maybeSeal(inv inventory.Inventory, secret inventory.Secret, v *viper.Viper, l *slog.Logger) error {
+func maybeSeal(s sealer, inv inventory.Inventory, secret inventory.Secret, v *viper.Viper, l *slog.Logger) error {
 	ansibleDir := v.GetString("ansible")
 
 	secretFile := filepath.Join(ansibleDir, inv.SecretsDir, secret.Source)
@@ -74,28 +85,48 @@ func maybeSeal(inv inventory.Inventory, secret inventory.Secret, v *viper.Viper,
 	}
 	defer func(f *os.File) { _ = f.Close() }(fOut)
 
-	err = sealer(
-		fOut,
-		fIn,
-		secret.Namespace,
-		v.GetString("controller-name"),
-		v.GetString("controller-namespace"),
-	)
+	err = s.seal(fOut, fIn, secret.Namespace)
 	l.Debug("kubeseal result", "err", err)
 	return err
 }
 
-// allow to override kubeseal during unit testing
-var sealer = doSeal
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func doSeal(w io.Writer, r io.Reader, namespace, controllerName, controllerNamespace string) error {
-	cmd := exec.Command("/usr/local/bin/kubeseal",
-		"--controller-name", controllerName,
-		"--controller-namespace", controllerNamespace,
-		"--namespace", namespace,
-		"-o", "yaml",
-	)
-	cmd.Stdin = r
-	cmd.Stdout = w
-	return cmd.Run()
+type kubeSealer struct {
+	clientConfig        kubeseal.ClientConfig
+	controllerNamespace string
+	controllerName      string
+	publicKey           *rsa.PublicKey
+}
+
+func newKubeSealer(controllerNamespace, controllerName string) *kubeSealer {
+	return &kubeSealer{
+		clientConfig:        initClient(),
+		controllerNamespace: controllerNamespace,
+		controllerName:      controllerName,
+	}
+}
+
+func initClient() clientcmd.ClientConfig {
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	loadingRules.DefaultClientConfig = &clientcmd.DefaultClientConfig
+	return clientcmd.NewInteractiveDeferredLoadingClientConfig(loadingRules, nil, nil)
+}
+
+func (s *kubeSealer) getPublicKey() error {
+	r, err := kubeseal.OpenCert(context.Background(), s.clientConfig, "sealed-secrets", "sealed-secrets", "")
+	if err == nil {
+		s.publicKey, err = kubeseal.ParseKey(r)
+		_ = r.Close()
+	}
+	return err
+}
+
+func (s *kubeSealer) seal(w io.Writer, r io.Reader, namespace string) error {
+	if s.publicKey == nil {
+		if err := s.getPublicKey(); err != nil {
+			return err
+		}
+	}
+	return kubeseal.Seal(s.clientConfig, "yaml", r, w, scheme.Codecs, s.publicKey, v1alpha1.DefaultScope, true, "", namespace)
 }
